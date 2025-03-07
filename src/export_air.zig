ba24cbe1dd687eba45bd4640998002f06cedc85e
Patrick Wickenhaeuser <35903594+patrickwick@users.noreply.github.com>
2025-03-07 06:51:13 +0100

    AIR export: create test compilation unit

--- src/export_air.zig
+++ src/export_air.zig
@@ -1,10 +1,12 @@
 // This is an experiment in exporting the analyzed intermediate representation (AIR)
 // in a minimal lossless form to be used in external static analysis tools.
 const std = @import("std");
+const builtin = @import("builtin");
 
 const Air = @import("Air.zig");
 const Compilation = @import("Compilation.zig");
 const Liveness = @import("Liveness.zig");
+const Package = @import("Package.zig");
 const Zcu = @import("Zcu.zig");
 
 /// Export AIR main body.
@@ -26,50 +28,134 @@ pub fn exportAirInst(inst: Air.Inst.Index, pt: Zcu.PerThread, air: Air, liveness
     }
 }
 
-// Tests from here on
+// Test code here on.
 const t = std.testing;
 
+// Create a test compilation unit for testing without source files. Simulates what happesn in src/main.zig.
 const TestCompilationUnit = struct {
-    zcu: *Zcu,
+    compilation: *Compilation,
     allocator: std.mem.Allocator,
     arena: std.heap.ArenaAllocator,
+    thread_pool: *std.Thread.Pool,
 
     pub fn init(allocator: std.mem.Allocator) !@This() {
         var arena = std.heap.ArenaAllocator.init(allocator);
         errdefer arena.deinit();
+        const arena_allocator = arena.allocator();
 
-        const zcu = try t.allocator.create(Zcu);
-        errdefer t.allocator.free(zcu);
+        const thread_stack_size = 32 << 20;
+        const thread_pool = try allocator.create(std.Thread.Pool);
+        errdefer allocator.destroy(thread_pool);
 
-        const comp = try t.allocator.create(Compilation);
-        errdefer t.allocator.free(comp);
+        const thread_count = 1;
+        try thread_pool.*.init(.{
+            .allocator = allocator,
+            .n_jobs = thread_count,
+            .track_ids = true,
+            .stack_size = thread_stack_size,
+        });
+        errdefer thread_pool.deinit();
 
-        comp = Compilation{
-            .gpa = allocator,
-            .zcu = zcu,
-            .arena = arena.allocator(),
+        const resolved_target = Package.Module.ResolvedTarget{
+            .result = try std.zig.system.resolveTargetQuery(.{}),
+            .is_native_os = true,
+            .is_native_abi = true,
+            .llvm_cpu_features = null,
         };
 
-        zcu = Zcu{
-            .gpa = t.allocator,
-            .comp = comp,
-        };
+        const root_module = try Package.Module.create(arena_allocator, .{
+            .global_cache_directory = std.Build.Cache.Directory.cwd(),
+            .paths = .{
+                .root = .{
+                    .root_dir = std.Build.Cache.Directory.cwd(),
+                },
+                .root_src_path = "src/main.zig",
+            },
+            .fully_qualified_name = "root",
+            .cc_argv = &.{},
+            .inherited = .{
+                .resolved_target = resolved_target,
+                .stack_check = false,
+                .stack_protector = 0,
+                .no_builtin = true,
+            },
+            .global = try Compilation.Config.resolve(.{
+                .output_mode = .Exe,
+                .resolved_target = resolved_target,
+                .is_test = false,
+                .have_zcu = true,
+                .emit_bin = true,
+            }),
+            .parent = null,
+            .builtin_mod = null, // NOTE: null will create a new builtin submodule on creation
+            .builtin_modules = null,
+        });
 
-        return .{ .zcu = zcu, .allocator = allocator, .arena = arena };
+        const compilation = try Compilation.create(allocator, arena_allocator, .{
+            .local_cache_directory = std.Build.Cache.Directory.cwd(),
+            .global_cache_directory = std.Build.Cache.Directory.cwd(),
+            .thread_pool = thread_pool,
+            .zig_lib_directory = .{ .path = "./", .handle = std.fs.cwd() },
+            .config = .{
+                .have_zcu = true,
+                .output_mode = .Exe,
+                .link_mode = .static,
+                .link_libc = false,
+                .link_libcpp = false,
+                .link_libunwind = false,
+                .any_c_source_files = false,
+                .any_unwind_tables = false,
+                .any_non_single_threaded = false,
+                .any_error_tracing = false,
+                .any_sanitize_thread = false,
+                .any_sanitize_c = false,
+                .any_fuzz = false,
+                .pie = true,
+                .use_llvm = false,
+                .use_lib_llvm = false,
+                .use_lld = false,
+                .c_frontend = .aro,
+                .lto = .none,
+                .wasi_exec_model = .command,
+                .import_memory = false,
+                .export_memory = false,
+                .shared_memory = false,
+                .is_test = false,
+                .debug_format = .{ .dwarf = .@"64" },
+                .root_optimize_mode = .Debug,
+                .root_strip = false,
+                .root_error_tracing = false,
+                .dll_export_fns = false,
+                .rdynamic = false,
+                .san_cov_trace_pc_guard = false,
+            },
+            .root_mod = root_module,
+            .root_name = "<root_name>",
+            .emit_bin = null,
+        });
+        errdefer compilation.destroy();
+
+        return .{
+            .compilation = compilation,
+            .allocator = allocator,
+            .arena = arena,
+            .thread_pool = thread_pool,
+        };
     }
 
     pub fn deinit(self: *@This()) void {
+        self.compilation.destroy();
+        self.thread_pool.*.deinit();
+        self.allocator.destroy(self.thread_pool);
         self.arena.deinit();
-        self.allocator.free(self.zcu.comp);
-        self.allocator.free(self.zcu);
     }
 };
 
 test exportAir {
-    const test_unit = try TestCompilationUnit.init(t.allocator);
+    var test_unit = try TestCompilationUnit.init(t.allocator);
     defer test_unit.deinit();
 
-    const pt = Zcu.PerThread{ .tid = 0, .zcu = test_unit.zcu };
+    const pt = Zcu.PerThread{ .tid = .main, .zcu = test_unit.compilation.zcu.? };
     const instructions = std.MultiArrayList(Air.Inst){};
 
     const air = Air{
